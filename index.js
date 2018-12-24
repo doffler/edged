b97394339a55d5d6c64cf42dd95e1d87ef7e5567
ben <me@jaeyunkim.com>
2018-12-24 10:59:13 +0900

    initial server using socket.io

---
+++ index.js
@@ -0,0 +1,44 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+/* GET home page. */
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+	socket.on('login', (data) => {
+		console.log(`Client logged-in:\nuserid: ${data.userid}`);
+		socket.userid = data.userid;
+	});
+
+	socket.on('req', (data) => {
+		console.log(`Request from ${socket.userid} with\nipfs: ${data.ipfs}\ninput: ${data.input}\nenv: ${data.env}\nbounty: ${data.bounty}`);
+
+		setTimeout(() => {
+			var result = "ReSuLt";
+			console.log(`result for ${socket.userid}: ${result}`);
+			var msg = {
+				from: {
+					userid: socket.userid
+				},
+				result: result
+			};
+			socket.emit('result', msg);
+		}, 3000);
+	});
+
+	// force client disconnect from server
+	socket.on('forceDisconnect', () => {
+		socket.disconnect();
+	})
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected: ' + socket.name);
+	});
+});
+
+server.listen(3000, () => {
+	console.log('Socket IO server listening on port 3000');
+});
